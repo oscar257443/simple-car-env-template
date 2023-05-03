6c8c2d622d6637e25190c7aac4ed13c372d6a4d9
oscar257443 <oscargullickson@gmail.com>
2023-05-03 20:25:32 +1000

    Att 7

--- simple_driving/resources/Obstacle.py
+++ simple_driving/resources/Obstacle.py
@@ -5,5 +5,5 @@ import os
 class Obstacle:
     def __init__(self, client):
         f_name = os.path.join(os.path.dirname(__file__), 'simpleObstacle.urdf')
-        self.goal = client.loadURDF(fileName=f_name,
+        self.Obstacle = client.loadURDF(fileName=f_name,
                    basePosition=[4, 3, 0])
